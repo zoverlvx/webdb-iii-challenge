c44e5e228ee50a46dac228182b3967b6811ae324
Zach Overhulser <zoverhulser@gmail.com>
2019-05-16 17:38:31 +0000

    Completes MVP

--- routes/cohortsRouter.js
+++ routes/cohortsRouter.js
@@ -8,6 +8,20 @@ router.get("/", (req, res) => {
     }).catch(err => handleRes(res, 500, {error: "There was an error retrieving the cohorts data."}))
 });
 
+router.get("/:id", (req, res) => {
+    db("cohorts")
+      .where({id: req.params.id})
+      .first()
+      .then(cohort => {
+	  if (cohort) {
+	      handleRes(res, 200, {success: true, cohort});
+	  }
+          if (!cohort) {
+	      handleRes(res, 404, {success: false, message: `Cohort with id of ${req.params.id} not found`})
+	  }
+      }).catch(err => handleRes(res, 500, err))
+});
+
 router.post("/", (req, res) => {
     const {name} = req.body;
     if (!name || typeof name !== "string") {
@@ -23,5 +37,38 @@ router.post("/", (req, res) => {
     }
 });
 
+router.put("/:id", (req, res) => {
+    const {name} = req.body;
+    if (!name || typeof name !== "string") {
+	handleRes(res, 400, {success: false, message: "Must provide a name value"})
+    }
+    if (typeof name === "string") {
+	db("cohorts")
+	  .where({id: req.params.id})
+	  .update({name}, id)
+	  .then(count => {
+	      if(count > 0) {
+  		  handleRes(res, 200, {success: true, message: "Record has been updated"});
+	      }
+	      if (count < 0) {
+		  handleRes(res, 404, {success: false, message: `Cohort with if of ${req.params.id} not found`})
+	      }
+	  }).catch(err => handleRes(res, 500, err))
+    }
+});
+
+router.delete("/:id", (req, res) => {
+    db("cohorts")
+      .where({id: req.params.id})
+      .del()
+      .then(count => {
+	  if (count > 0) {
+	      handleRes(res, 204, {success: true})
+	  }
+	  if (count < 0) {
+	      handleRes(res, 404, {success: false, message: `Cohort of id ${req.params.id} not found`});
+	  }
+      }).catch(err => handleRes(res, 500, err))
+});
 
 module.exports = router;
---
+++ scripts/test_update.sh
@@ -0,0 +1,1 @@
+curl -d '{"name": "'"$2"'"}' -H "Content-Type: application/json" -X PUT "https://lambda-school-2-zoverlvx.c9users.io/api/cohorts/$1"
